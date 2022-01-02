2fc1ef02272f6abbd141f250c9b013119aa3ebc3
Hannes Leskela <hleskela@kth.se>
2022-01-02 18:43:34 +0100

    Updates

---
+++ precache-manifest.611a23f3e7f0e3f081faf0d8dc43cd46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "46ac4343cda2852e08b1",
+    "url": "/my-app/static/css/main.18de3ae7.chunk.css"
+  },
+  {
+    "revision": "46ac4343cda2852e08b1",
+    "url": "/my-app/static/js/main.46ac4343.chunk.js"
+  },
+  {
+    "revision": "c6e0fb0ee92a2f4d1ae0",
+    "url": "/my-app/static/js/1.c6e0fb0e.chunk.js"
+  },
+  {
+    "revision": "103959f98ddbb167af69",
+    "url": "/my-app/static/js/2.103959f9.chunk.js"
+  },
+  {
+    "revision": "d9433759fffda5124312",
+    "url": "/my-app/static/js/runtime~main.d9433759.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/my-app/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "5b2d4108c3575624f33f865e4f7a271f",
+    "url": "/my-app/index.html"
+  }
+];
